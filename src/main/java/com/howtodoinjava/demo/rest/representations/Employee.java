f2b1867169d10fd0eb5c65822d77f04cf8de5dd3
Nimra Khan <nimra.zhcet@gmail.com>
2016-11-15 11:33:31 +0530

    Intial commit

---
+++ src/main/java/com/howtodoinjava/demo/rest/representations/Employee.java
@@ -0,0 +1,69 @@
+package com.howtodoinjava.demo.rest.representations;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ * Created by vgarg on 11/10/16.
+ */
+public class Employee {
+    @NotNull
+    private Integer id;
+    @NotBlank @Length(min=2, max=255)
+    private String firstName;
+    @NotBlank @Length(min=2, max=255)
+    private String lastName;
+    @Pattern(regexp=".+@.+\\.[a-z]+")
+    private String email;
+
+    public Employee(){
+    }
+
+    public Employee(Integer id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Emplyee [id=" + id + ", firstName=" + firstName + ", lastName="
+                + lastName + ", email=" + email + "]";
+    }
+}
